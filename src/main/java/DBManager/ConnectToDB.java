77ca6173476140eae43137403591b3002ab5669b
Arunendra Dubey <Arunendra.Dubey@microfocus.com>
2021-07-26 15:02:50 +0530

    Pathology

--- src/main/java/DBManager/ConnectToDB.java
+++ src/main/java/DBManager/ConnectToDB.java
@@ -4,39 +4,38 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+
 public class ConnectToDB {
 	Statement statement;
 	Connection connection;
-	public ConnectToDB(){
-		this.statement=this.creatConnection();
-	}  
-    public Statement creatConnection() {
-        String connectionUrl = "jdbc:sqlserver://10.204.103.84:1433;"
-                        + "database=pathDB;"
-                        + "user=sqlArun;"
-                        + "password=Control123;"
-                        + "loginTimeout=30;";
-         try{
-        		connection = DriverManager.getConnection(connectionUrl);
-        		statement = connection.createStatement(); 
-                return statement;
-        }
-        catch (SQLException e) {
-            System.out.print("Unable to connect............");
-            return null;
-        }
-    }
-    
-    public Statement getConnection(){
-    	return this.statement;
-    }
-    
-    public void close() {
-    	try {
+
+	public ConnectToDB() {
+		this.statement = this.creatConnection();
+	}
+
+	public Statement creatConnection() {
+		String connectionUrl = "jdbc:sqlserver://10.204.103.84:1433;" + "database=pathDB;" + "user=sqlArun;"
+				+ "password=Control123;" + "loginTimeout=30;";
+		try {
+			connection = DriverManager.getConnection(connectionUrl);
+			statement = connection.createStatement();
+			return statement;
+		} catch (SQLException e) {
+			System.out.print("Unable to connect............");
+			return null;
+		}
+	}
+
+	public Statement getConnection() {
+		return this.statement;
+	}
+
+	public void close() {
+		try {
 			statement.close();
 			connection.close();
 		} catch (SQLException e) {
 			System.out.print("Unable to close DB connection");
 		}
-    }
+	}
 }
